89561d02c25c2f4133df439ffc8bdc96cc0bfd07
Bill845514379 <51468896+Bill845514379@users.noreply.github.com>
2021-12-31 13:28:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ for test_id in range(len(seeds)):
             output = net(pos, neg, batch_x)
 
             loss_fn = torch.nn.MSELoss(reduce=True, size_average=True)
-            loss = loss_fn(output[0], torch.tensor([0.0]).to(device)) + loss_fn(output[1], torch.tensor([50.0]).to(device))
+            loss = loss_fn(output[0], torch.tensor([0.0]).to(device)) + loss_fn(output[1], torch.tensor([1.0]).to(device))
             loss.backward()
             torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
             optimizer.step()  # 更新权重
